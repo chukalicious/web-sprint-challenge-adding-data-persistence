38dca6fccd64d90e718e227b486c15b473961bd7
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-07 11:22:10 -0800

    more setup on index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here
+const express = require("express");
+
+const server = express();
+
+//require routers here
+const Router = require("./path-to-router");
+// import other routers
+
+server.use(express.json());
+
+//use your routers below
+server.use(`api/path/to/endpoint`, Router);
+//other router
+//another router
+
+//export server to index.js
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // start your server here
+
+require("dotenv").config();
+
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
